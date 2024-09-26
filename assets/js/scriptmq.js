1c9675717e5491603a2357c49c9151a6411d0be9
Vitoria '@ <sena61673@gmail.com>
2024-09-26 14:48:07 -0300

    portifolio Mobile Firt

---
+++ assets/js/scriptmq.js
@@ -0,0 +1,8 @@
+// Esse código serve para monitorar o tamanha de tela 
+function mudoutamanho() {
+    //itens.style.display = 'inline-blocr'
+    Intens.style.dislpay = 'block'
+ } else {
+    itens.style.dislpay = 'none'
+ }
+}
