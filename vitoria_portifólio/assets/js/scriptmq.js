b11b7811d1ed5892d15f2ebd4f28fabf34f71fbd
Vitoria '@ <sena61673@gmail.com>
2024-10-09 16:48:21 -0300

    Add files via upload

---
+++ vitoria_portifólio/assets/js/scriptmq.js
@@ -0,0 +1,16 @@
+// function mudoutamanho() {
+//     //itens.style.display = 'inline-block'
+//     Intens.style.dislpay = 'block'
+//  } else {
+//     itens.style.dislpay = 'none'
+//  }
+
+
+ function mudouTamanho() {
+   if (window.innerWidth >= 768) {
+       //itens.style.display = 'inline-block'
+       itens.style.display = 'block'
+   } else {
+       itens.style.display = 'none'
+   }
+}
