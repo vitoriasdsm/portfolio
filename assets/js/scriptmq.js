646f60b0ab335c49e8fee5afe06947b1bd88da49
Vitoria '@ <sena61673@gmail.com>
2025-03-11 11:39:08 -0300

    Add files via upload

--- assets/js/scriptmq.js
+++ assets/js/scriptmq.js
@@ -1,8 +1,16 @@
-// Esse código serve para monitorar o tamanha de tela 
-function mudoutamanho() {
-    //itens.style.display = 'inline-blocr'
-    Intens.style.dislpay = 'block'
- } else {
-    itens.style.dislpay = 'none'
- }
+// function mudoutamanho() {
+//     //itens.style.display = 'inline-block'
+//     Intens.style.dislpay = 'block'
+//  } else {
+//     itens.style.dislpay = 'none'
+//  }
+
+
+ function mudouTamanho() {
+   if (window.innerWidth >= 768) {
+       //itens.style.display = 'inline-block'
+       itens.style.display = 'block'
+   } else {
+       itens.style.display = 'none'
+   }
 }
